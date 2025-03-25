2390bf15a1df470b7353303491de96b9ff5b3f98
AmEr-v <114492185+AmEr-v@users.noreply.github.com>
2025-03-25 11:37:58 +0100

    edit ini.py

--- ini.py
+++ ini.py
@@ -1,10 +1,10 @@
 import subprocess
+import sys
 
 def main():
     print("Initializing the scanning process...")
     try:
-        # Execute FirstScan.py using subprocess
-        subprocess.run(["python", "FirstScan.py"], check=True)
+        subprocess.run([sys.executable, "FirstScan.py"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error executing FirstScan.py: {e}")
     except FileNotFoundError:
